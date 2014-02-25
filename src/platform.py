c26397251a12f36130c8b98a053a1911dc419631
Martin Fiedler <keyj@users.sourceforge.net>
2014-02-25 17:35:50 +0000

    gl2: win32 platform fix
    git-svn-id: http://svn.emphy.de/impressive/branches/gl2/impressive@132 58003caa-664d-4c56-a378-6d187ff66ee1

--- src/platform.py
+++ src/platform.py
@@ -30,7 +30,7 @@ class Platform_PyGame(object):
 
     def LoadOpenGL(self):
         try:
-            sdl = CDLL(ctypes.util.find_library("SDL"), RTLD_GLOBAL)
+            sdl = CDLL(ctypes.util.find_library("SDL") or "SDL", RTLD_GLOBAL)
             get_proc_address = CFUNCTYPE(c_void_p, c_char_p)(('SDL_GL_GetProcAddress', sdl))
         except OSError:
             raise ImportError("failed to load the SDL library")
